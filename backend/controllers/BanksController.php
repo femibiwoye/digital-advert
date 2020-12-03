8f09395107ec5ad499122f8a591fec336ae1df8a
yhunghabey <yhung.habey1994@gmail.com>
2020-12-03 08:21:09 +0100

    authentication

--- backend/controllers/BanksController.php
+++ backend/controllers/BanksController.php
@@ -8,6 +8,7 @@ use common\models\BanksSearch;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
+use yii\filters\AccessControl;
 
 /**
  * BanksController implements the CRUD actions for Banks model.
@@ -20,6 +21,22 @@ class BanksController extends Controller
     public function behaviors()
     {
         return [
+            'access' => [
+                'class' => AccessControl::className(),
+                'only' => ['logout', 'signup'],
+                'rules' => [
+                    [
+                        'actions' => ['signup'],
+                        'allow' => true,
+                        'roles' => ['?'],
+                    ],
+                    [
+                        'actions' => ['logout'],
+                        'allow' => true,
+                        'roles' => ['@'],
+                    ],
+                ],
+            ],
             'verbs' => [
                 'class' => VerbFilter::className(),
                 'actions' => [
