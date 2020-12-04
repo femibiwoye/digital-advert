9dfd0cebe87599f054fb55ae5e2720dc0b61835c
tolu-six <tolu58@gmail.com>
2020-12-04 21:20:25 +0100

    Notification

---
+++ frontend/modules/affiliate/views/notification/index.php
@@ -0,0 +1,34 @@
+<?php
+
+use yii\helpers\Html;
+use yii\grid\GridView;
+
+/* @var $this yii\web\View */
+/* @var $searchModel app\models\NotificationSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Notifications';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="notification-index">
+
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'title',
+            //'description:ntext',
+            //'generality',
+            //'created_at',
+
+            ['class' => 'yii\grid\ActionColumn',
+
+                'template' => '{view}'
+            ],
+        ],
+    ]); ?>
+
+
+</div>
---
+++ frontend/modules/affiliate/views/notification/view.php
@@ -0,0 +1,30 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\DetailView;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\Notification */
+
+$this->title = $model->title;
+$this->params['breadcrumbs'][] = ['label' => 'Notifications', 'url' => ['index']];
+$this->params['breadcrumbs'][] = $this->title;
+\yii\web\YiiAsset::register($this);
+?>
+<div class="notification-view">
+
+
+    <?= DetailView::widget([
+        'model' => $model,
+        'attributes' => [
+            
+            'title',
+            'description:ntext',
+            [
+                'label' => '',
+                'value' => 'created_at'
+            ],
+        ],
+    ]) ?>
+
+</div>
