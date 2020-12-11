61b1051dbff48df105eade4fa97af1fde55fca86
yhunghabey <yhung.habey1994@gmail.com>
2020-12-11 13:31:27 +0100

    cleaned commented forms

--- backend/views/banks/_form.php
+++ backend/views/banks/_form.php
@@ -12,19 +12,12 @@ use yii\widgets\ActiveForm;
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <?//= $form->field($model, 'created_at')->textInput() ?>
-
-    <?//= $form->field($model, 'updated_at')->textInput() ?>
-
-    <?//= $form->field($model, 'user_id')->textInput() ?>
 
     <?= $form->field($model, 'bank_name')->textInput(['maxlength' => true]) ?>
 
     <?= $form->field($model, 'account_name')->textInput(['maxlength' => true]) ?>
 
     <?= $form->field($model, 'account_number')->textInput(['maxlength' => true]) ?>
-
-    <?//= $form->field($model, 'approval_status')->textInput() ?>
 
     <div class="form-group">
         <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
--- backend/views/wallet-histories/_form.php
+++ backend/views/wallet-histories/_form.php
@@ -23,7 +23,6 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'operation')->textInput(['maxlength' => true]) ?>
 
-    <?//= $form->field($model, 'IP')->textInput(['maxlength' => true]) ?>
 
     <div class="form-group">
         <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
